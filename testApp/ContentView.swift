85a733b4d82a067f57b75b80ba3fcf485c2814f7
MAC - EAAA <dmumac19@dmu-mac-19.local>
2022-08-22 12:26:37 +0200

    Initial commit

---
+++ testApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  testApp
+//
+//  Created by dmu mac 19 on 22/08/2022.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testApp/testAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by dmu mac 19 on 22/08/2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
